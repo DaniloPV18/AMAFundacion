99e19a55affe98fe886976f3434d4e0d9f46040e
AlexSalazarMVC <alex.salazars@ug.edu.ec>
2024-07-15 23:07:31 -0500

    duplicidad de registro

--- src/Backend/src/FundacionAMA.Infrastructure/Migrations/20240716034841_correccionbrigada.Designer.cs
+++ src/Backend/src/FundacionAMA.Infrastructure/Migrations/20240716034841_correccionbrigada.Designer.cs
@@ -4,6 +4,7 @@ using FundacionAMA.Infrastructure.Persistence.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FundacionAMA.Infrastructure.Migrations
 {
     [DbContext(typeof(AMADbContext))]
-    partial class AMADbContextModelSnapshot : ModelSnapshot
+    [Migration("20240716034841_correccionbrigada")]
+    partial class correccionbrigada
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
